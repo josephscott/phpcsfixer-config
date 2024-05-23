31f7a0d463590c55a6691a50042f8cff9ff5d764
Joseph Scott <joseph@josephscott.org>
2024-05-22 21:39:40 -0600

    Adust variable name

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,5 +3,5 @@ declare( strict_types = 1 );
 
 require __DIR__ . '/src/josephscott/phpcsfixer-config.php';
 
-$js_config = new JosephScott\PHPCSFixer_Config( __DIR__ );
-return $js_config->get_config();
+$fixer_config = new JosephScott\PHPCSFixer_Config( __DIR__ );
+return $fixer_config->get_config();
