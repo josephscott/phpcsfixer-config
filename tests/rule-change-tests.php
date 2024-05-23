2f790e4791e006b99a0003fe1ab3a2be9c47f5d8
Joseph Scott <joseph@josephscott.org>
2024-05-22 22:22:07 -0600

    Update variable names

--- tests/rule-change-tests.php
+++ tests/rule-change-tests.php
@@ -2,10 +2,10 @@
 declare( strict_types = 1 );
 
 test( 'rule-change', function () {
-	$js_config = new JosephScott\PHPCSFixer_Config( __DIR__ );
+	$fixer_config = new JosephScott\PHPCSFixer_Config( __DIR__ );
 
-	$js_config->rules['array_indention'] = false;
-	$new_config = $js_config->get_config();
+	$fixer_config->rules['array_indention'] = false;
+	$new_config = $fixer_config->get_config();
 
 	$string_config = var_export( $new_config, true );
 	expect( $string_config )->toContain( "'array_indention' => false," );
